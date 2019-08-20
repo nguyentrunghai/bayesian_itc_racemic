8360d5cf6c6c102f5da6cc03e7e9434e21f5f3bc
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-08-20 22:14:34 +0700

    parse data files

--- scripts/run_plot_histograms.py
+++ scripts/run_plot_histograms.py
@@ -3,7 +3,21 @@ to compare histogram of DG, DH, P0 and Ls between twocomponent and racemicmixtur
 """
 
 import argparse
+import glob
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument( "--bitc_mcmc_dir",     type=str, default="bitc_mcmc")
+parser.add_argument("--twocomponent_mcmc_dir", type=str, default="twocomponent")
+parser.add_argument("--racemicmixture_mcmc_dir", type=str, default="racemicmixture")
 args = parser.parse_args()
+
+TRACES_FILE_NAME = "traces.pickle"
+
+twocomponent_traces_files = glob.glob(os.path.join(args.twocomponent_mcmc_dir, "*", TRACES_FILE_NAME))
+racemicmixture_traces_files = glob.glob(os.path.join(args.racemicmixture_mcmc_dir, "*", TRACES_FILE_NAME))
+
+twocomponent_traces_files = {os.path.basename(os.path.dirname(f)): f for f in twocomponent_traces_files}
+racemicmixture_traces_files = {os.path.basename(os.path.dirname(f)): f for f in racemicmixture_traces_files}
+print("twocomponent_traces_files", twocomponent_traces_files)
+print("racemicmixture_traces_files", racemicmixture_traces_files)
+
