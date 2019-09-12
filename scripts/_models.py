df6eeafebac0ce34ad3800300541b96f97137fdc
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-12 21:14:56 +0700

    add function equilibrium_concentrations

--- scripts/_models.py
+++ scripts/_models.py
@@ -70,3 +70,37 @@ def heats_TwoComponentBindingModel(V0, DeltaVn, P0, Ls, DeltaG, DeltaH, DeltaH_0
         q_n[n] = (DeltaH * V0 * (PLn[n] - d * PLn[n - 1])) * 1000 + DeltaH_0
 
     return q_n
+
+
+# copied from method equilibrium_concentrations of class RacemicMixtureBindingModel in bayesian_itc/bitc/models.py
+def equilibrium_concentrations(Kd1, Kd2, C0_R, C0_L1, C0_L2, V):
+    """
+    REF: Zhi-Xin Wang An exact mathematical expression for describing competitive
+                        binding of two different ligands to a protein molecule
+                        FEBS Letters 360 (1995) 111-114
+
+    Kd1, Kd2        :   disociation constants of ligand 1 and 2, respectively (M)
+    C0_R            :   total concentration receptor   (M)
+    C0_L1, C0_L2    :   total concentrations of ligand 1 and ligand 2, respectively (M)
+    V               :   volume of sample cell (L)
+    return
+                [RL1, RL2]  complex concentrations (M)
+    """
+    assert (Kd1 > 0) and (Kd2 > 0), "Kd1 and Kd2 must be positive. Kd1=%0.5f, Kd2=%0.5f" %(Kd1, Kd2)
+    assert (C0_R > 0) and (C0_L1 > 0) and (C0_L2 > 0), "concentrations must be positive (R, L1, L2) = (%0.5f, %0.5f, %0.5f)" %(C0_R, C0_L1, C0_L2)
+    assert V > 0, "volume must be positive"
+
+    a = Kd1 + Kd2 + C0_L1 + C0_L2 - C0_R
+
+    b = Kd2*(C0_L1 - C0_R) + Kd1*(C0_L2 - C0_R) + Kd1*Kd2
+
+    c = -Kd1 * Kd2 * C0_R
+
+    d = np.sqrt(a*a - 3*b)
+
+    theta = np.arccos((-2.*a**3 + 9.*a*b - 27.*c) / (2.*d**3))
+
+    RL1 = C0_L1*(2.*d*np.cos(theta/3.) - a) / (3.*Kd1 + (2.*d*np.cos(theta/3.) - a))
+    RL2 = C0_L2*(2.*d*np.cos(theta/3.) - a) / (3.*Kd2 + (2.*d*np.cos(theta/3.) - a))
+
+    return RL1, RL2
