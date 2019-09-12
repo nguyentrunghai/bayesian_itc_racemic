e8dd8d6cc7713707cc368d0339580d39dc73f377
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-12 20:44:04 +0700

    add file contain functions of model heat

---
+++ scripts/_models.py
@@ -0,0 +1,70 @@
+"""
+define different heat models
+"""
+
+
+# copied from bayesian_itc
+def expected_injection_heats(V0, DeltaVn, P0, Ls, DeltaG, DeltaH, DeltaH_0, beta, N):
+    """
+    Expected heats of injection for two-component binding model.
+
+    ARGUMENTS
+    V0 - cell volume (liter)
+    DeltaVn - injection volumes (liter)
+    P0 - Cell concentration (millimolar)
+    Ls - Syringe concentration (millimolar)
+    DeltaG - free energy of binding (kcal/mol)
+    DeltaH - enthalpy of binding (kcal/mol)
+    DeltaH_0 - heat of injection (cal)
+    beta - inverse temperature * gas constant (mole / kcal)
+    N - number of injections
+
+    Returns
+    -------
+    expected injection heats (calorie)
+
+    """
+
+    Kd = numpy.exp(beta * DeltaG)   # dissociation constant (M)
+    N = N
+
+    # Compute complex concentrations.
+    # Pn[n] is the protein concentration in sample cell after n injections
+    # (M)
+    Pn = numpy.zeros([N])
+    # Ln[n] is the ligand concentration in sample cell after n injections
+    # (M)
+    Ln = numpy.zeros([N])
+    # PLn[n] is the complex concentration in sample cell after n injections
+    # (M)
+    PLn = numpy.zeros([N])
+    dcum = 1.0  # cumulative dilution factor (dimensionless)
+    for n in range(N):
+        # Instantaneous injection model (perfusion)
+        # TODO: Allow injection volume to vary for each injection.
+        # dilution factor for this injection (dimensionless)
+        d = 1.0 - (DeltaVn[n] / V0)
+        dcum *= d  # cumulative dilution factor
+        # total quantity of protein in sample cell after n injections (mol)
+        P = V0 * P0 * 1.e-3 * dcum
+        # total quantity of ligand in sample cell after n injections (mol)
+        L = V0 * Ls * 1.e-3 * (1. - dcum)
+        # complex concentration (M)
+        PLn[n] = (0.5 / V0 * ((P + L + Kd * V0) - ((P + L + Kd * V0) ** 2 - 4 * P * L) ** 0.5))
+        # free protein concentration in sample cell after n injections (M)
+        Pn[n] = P / V0 - PLn[n]
+        # free ligand concentration in sample cell after n injections (M)
+        Ln[n] = L / V0 - PLn[n]
+
+    # Compute expected injection heats.
+    # q_n_model[n] is the expected heat from injection n
+    q_n = numpy.zeros([N])
+    # Instantaneous injection model (perfusion)
+    # first injection
+    q_n[0] = (DeltaH * V0 * PLn[0])*1000 + DeltaH_0
+    for n in range(1, N):
+        d = 1.0 - (DeltaVn[n] / V0)  # dilution factor (dimensionless)
+        # subsequent injections
+        q_n[n] = (DeltaH * V0 * (PLn[n] - d * PLn[n - 1])) * 1000 + DeltaH_0
+
+    return q_n
