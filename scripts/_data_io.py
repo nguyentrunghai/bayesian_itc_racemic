77cf935ed5223a88d52e07127193291e920f8073
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-12 22:45:11 +0700

    done class ITCExperiment

--- scripts/_data_io.py
+++ scripts/_data_io.py
@@ -107,3 +107,22 @@ class ITCExperiment:
         :param experimental_info_pickle: str, name of the pickle file
         """
         self._exper_info = pickle.load(open(experimental_info_pickle))
+
+    def get_target_temperature_kelvin(self):
+        return self._exper_info["target_temperature"].m
+
+    def get_number_injections(self):
+        return self._exper_info["number_of_injections"]
+
+    def get_cell_volume_liter(self):
+        return self._exper_info["cell_volume"].m_as("liter")
+
+    def get_injection_volumes_liter(self):
+        return [inj.volume.m_as("liter") for inj in self._exper_info["injections"]]
+
+    def get_syringe_concentration_milli_molar(self):
+        return self._exper_info["syringe_concentration"]["ligand"].m
+
+    def get_cell_concentration_milli_molar(self):
+        return self._exper_info["cell_concentration"]["macromolecule"].m
+
