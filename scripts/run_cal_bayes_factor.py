2c860b4068c875ef48d2902fe4a3e1c7b79ae522
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-13 15:55:32 +0700

    test

--- scripts/run_cal_bayes_factor.py
+++ scripts/run_cal_bayes_factor.py
@@ -16,7 +16,7 @@ parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
 parser.add_argument("--two_component_mcmc_dir", type=str,
                     default="/home/tnguye46/bayesian_itc_racemic/5.twocomponent_mcmc/lognomP0_lognomLs")
 
-parser.add_argument("--heat_dir", type=str, default="/home/tnguye46/bayesian_itc_racemic/3.dummy_itc_files")
+parser.add_argument("--heat_dir", type=str, default="/home/tnguye46/bayesian_itc_racemic/4.heat_in_origin_format")
 
 parser.add_argument("--exper_info_file", type=str, default="experimental_information.pickle")
 parser.add_argument("--mcmc_trace_file", type=str, default="traces.pickle")
