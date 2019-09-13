31fc00fc03837e22126ff9cd46ee2efd1fbdb3b9
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-13 16:16:03 +0700

    test

--- scripts/run_cal_bayes_factor.py
+++ scripts/run_cal_bayes_factor.py
@@ -8,6 +8,7 @@ import argparse
 import pickle
 
 from _data_io import ITCExperiment, load_heat_micro_cal
+from _models import average_likelihood_TwoComponentBindingModel, average_likelihood_RacemicMixtureBindingModel
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
@@ -27,18 +28,38 @@ parser.add_argument("--experiments", type=str,
 
 args = parser.parse_args()
 
+KB = 0.0019872041      # in kcal/mol/K
+
 experiments = args.experiments.split()
 
 for experiment in experiments:
     print(experiment)
     actual_heat_micro_cal = load_heat_micro_cal(os.path.join(args.heat_dir, experiment + ".DAT"))
 
-    exper_info_racemic_mixture = ITCExperiment(os.path.join(args.racemic_mixture_mcmc_dir,
-                                                            experiment, args.exper_info_file))
-    exper_info_two_component = ITCExperiment(os.path.join(args.two_component_mcmc_dir,
-                                                          experiment, args.exper_info_file))
+    exper_info_rmbm = ITCExperiment(os.path.join(args.racemic_mixture_mcmc_dir,
+                                                 experiment, args.exper_info_file))
+    exper_info_2cbm = ITCExperiment(os.path.join(args.two_component_mcmc_dir,
+                                                 experiment, args.exper_info_file))
 
-    trace_racemic_mixture = pickle.load(open(os.path.join(args.racemic_mixture_mcmc_dir,
-                                                          experiment, args.mcmc_trace_file)))
-    trace_two_component = pickle.load(open(os.path.join(args.two_component_mcmc_dir,
+    trace_rmbm = pickle.load(open(os.path.join(args.racemic_mixture_mcmc_dir,
+                                               experiment, args.mcmc_trace_file)))
+    trace_2cbm = pickle.load(open(os.path.join(args.two_component_mcmc_dir,
                                                         experiment, args.mcmc_trace_file)))
+
+    aver_likelihood_rmbm = average_likelihood_RacemicMixtureBindingModel(actual_heat_micro_cal,
+                                                                         V0=exper_info_rmbm.get_cell_volume_liter(),
+                                                                         DeltaVn=exper_info_rmbm.get_injection_volumes_liter(),
+                                                                         beta=1/KB/exper_info_rmbm.get_target_temperature_kelvin(),
+                                                                         n_injections=exper_info_rmbm.get_number_injections(),
+                                                                         mcmc_trace=trace_rmbm)
+
+    aver_likelihood_2cbm = average_likelihood_TwoComponentBindingModel(actual_heat_micro_cal,
+                                                                       V0=exper_info_2cbm.get_cell_volume_liter(),
+                                                                       DeltaVn=exper_info_2cbm.get_injection_volumes_liter(),
+                                                                       beta=1 / KB / exper_info_2cbm.get_target_temperature_kelvin(),
+                                                                       n_injections=exper_info_2cbm.get_number_injections(),
+                                                                       mcmc_trace=trace_2cbm)
+
+    print("aver_likelihood_rmbm", aver_likelihood_rmbm)
+    print("aver_likelihood_2cbm", aver_likelihood_2cbm)
+    print("ratio", aver_likelihood_rmbm / aver_likelihood_2cbm)
