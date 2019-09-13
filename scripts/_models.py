73f9dfb0ba074379dfd149e424a81dcf73367a6e
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-13 16:36:37 +0700

    fix bug in normal_likelihood()

--- scripts/_models.py
+++ scripts/_models.py
@@ -180,13 +180,14 @@ def normal_likelihood(q_actual, q_model, sigma):
     :param sigma: standard deviation
     :return: likelihood, float
 
-    log_likelihood = N/(4*\sigma^2) ln(2*\pi*\sigma^2) \sum_{i=1}^{N} epsilon_i^2
+    log_likelihood = -(N/2)\ln(2 \pi \sigma^2) - 1/(2 \sigma^2) \sum_{i=1}^N \epsilon^2
     """
     assert len(q_actual) == len(q_model), "q_actual and q_model must have the same len"
     sum_e_squared = np.sum((q_model - q_actual)**2)
 
     n_injections = len(q_actual)
-    log_likelihood = n_injections / (4.*sigma**2) * np.log(2 * np.pi * sigma**2) * sum_e_squared
+    sigma_2 = sigma**2
+    log_likelihood = - n_injections / 2 * np.log(2 * np.pi * sigma_2) - sum_e_squared / 2 / sigma_2
 
     return np.exp(log_likelihood)
 
