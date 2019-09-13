bec4bde75c0a926997bc964f955a663b708d4b6c
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-13 15:54:12 +0700

    fix bugs in args

--- scripts/run_cal_bayes_factor.py
+++ scripts/run_cal_bayes_factor.py
@@ -10,18 +10,18 @@ import pickle
 from _data_io import ITCExperiment, load_heat_micro_cal
 
 parser = argparse.ArgumentParser()
-parser.add_argument("racemic_mixture_mcmc_dir", type=str,
+parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
                     default="/home/tnguye46/bayesian_itc_racemic/6.racemicmixture_mcmc/lognomP0_lognomLs_narrowUniformRho")
 
-parser.add_argument("two_component_mcmc_dir", type=str,
+parser.add_argument("--two_component_mcmc_dir", type=str,
                     default="/home/tnguye46/bayesian_itc_racemic/5.twocomponent_mcmc/lognomP0_lognomLs")
 
-parser.add_argument("heat_dir", type=str, default="/home/tnguye46/bayesian_itc_racemic/3.dummy_itc_files")
+parser.add_argument("--heat_dir", type=str, default="/home/tnguye46/bayesian_itc_racemic/3.dummy_itc_files")
 
-parser.add_argument("exper_info_file", type=str, default="experimental_information.pickle")
-parser.add_argument("mcmc_trace_file", type=str, default="traces.pickle")
+parser.add_argument("--exper_info_file", type=str, default="experimental_information.pickle")
+parser.add_argument("--mcmc_trace_file", type=str, default="traces.pickle")
 
-parser.add_argument("experiments", type=str,
+parser.add_argument("--experiments", type=str,
                     default="Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
 
 
