593fbefbef74ce5815b70a80e7db98cf26f3545e
Trung Hai Nguyen <nguyentrungh@gmail.com>
2019-09-15 14:26:57 +0700

    load data

--- scripts/run_plot_actual_vs_model_heats.py
+++ scripts/run_plot_actual_vs_model_heats.py
@@ -2,11 +2,47 @@
 plot comparison between actual heats and model heats
 for model heats, we use maximum a posterior estimates of parameters
 """
+from __future__ import print_function
 
+import os
 import argparse
+import pickle
+
+from _data_io import ITCExperiment, load_heat_micro_cal
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
                     default="/home/tnguye46/bayesian_itc_racemic/6.racemicmixture_mcmc/lognomP0_lognomLs_narrowUniformRho")
 
+parser.add_argument("--two_component_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/5.twocomponent_mcmc/lognomP0_lognomLs")
+
+parser.add_argument("--heat_dir", type=str, default="/home/tnguye46/bayesian_itc_racemic/4.heat_in_origin_format")
+
+parser.add_argument("--exper_info_file", type=str, default="experimental_information.pickle")
+parser.add_argument("--mcmc_trace_file", type=str, default="traces.pickle")
+
+parser.add_argument("--experiments", type=str,
+                    default="Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
+
 args = parser.parse_args()
+
+KB = 0.0019872041      # in kcal/mol/K
+
+experiments = args.experiments.split()
+
+for experiment in experiments:
+    print(experiment)
+    actual_q_micro_cal = load_heat_micro_cal(os.path.join(args.heat_dir, experiment + ".DAT"))
+    actual_q_cal = actual_q_micro_cal * 10**(-6)
+
+    exper_info_2cbm = ITCExperiment(os.path.join(args.two_component_mcmc_dir,
+                                                 experiment, args.exper_info_file))
+    exper_info_rmbm = ITCExperiment(os.path.join(args.racemic_mixture_mcmc_dir,
+                                                 experiment, args.exper_info_file))
+
+    trace_2cbm = pickle.load(open(os.path.join(args.two_component_mcmc_dir,
+                                               experiment, args.mcmc_trace_file)))
+    trace_rmbm = pickle.load(open(os.path.join(args.racemic_mixture_mcmc_dir,
+                                               experiment, args.mcmc_trace_file)))
