745e1c2bc5fcbb8103173a03dccea1db53001544
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-09-27 16:14:19 +0700

    generate prior traces

---
+++ scripts/run_submit_mcmc_priors.py
@@ -0,0 +1,96 @@
+"""
+run mcmc sampling of priors
+"""
+
+from __future__ import print_function
+
+import os
+import sys
+import argparse
+import pickle
+
+from _models import sample_priors
+from _data_io import ITCExperiment, load_heat_micro_cal
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--exper_info_dir", type=str, default="5.twocomponent_mcmc")
+parser.add_argument("--exper_info_file", type=str, default="experimental_information.pickle")
+
+parser.add_argument("--heat_dir", type=str, default="4.heat_in_origin_format")
+parser.add_argument("--heat_file", type=str, default="heat.DAT")
+
+parser.add_argument("--dP0", type=float, default=0.1)      # cell concentration relative uncertainty
+parser.add_argument("--dLs", type=float, default=0.1)      # syringe concentration relative uncertainty
+
+parser.add_argument("--uniform_P0", action="store_true", default=False)
+parser.add_argument("--uniform_Ls", action="store_true", default=False)
+parser.add_argument("--concentration_range_factor", type=float, default=10.)
+
+parser.add_argument("--nsamples", type=int, default=1000000)
+parser.add_argument("--nburn", type=int, default=10000)
+parser.add_argument("--nthin", type=int, default=100)
+
+parser.add_argument("--experiments", type=str, default="Fokkens_1_c Fokkens_1_d")
+
+parser.add_argument("--out", type=str, default="traces.pickle")
+
+parser.add_argument("--submit",   action="store_true", default=False)
+
+args = parser.parse_args()
+
+
+
+if args.submit:
+    this_script = os.path.abspath(sys.argv[0])
+    experiments = args.experiments.split()
+
+    for experiment in experiments:
+
+        exper_info_file = os.path.join(args.exper_info_dir, experiment, args.experiment)
+        heat_file = os.path.join(args.heat_dir, experiment + ".DAT")
+
+        out_dir = experiment
+        if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
+        out_dir = os.path.abspath(out_dir)
+
+        qsub_file = os.path.join(out_dir, experiment + "_mcmc.job")
+        log_file = os.path.join(out_dir, experiment + "_mcmc.log")
+
+
+
+else:
+    exper_info_file = args.exper_info_file
+    print("Experimental info file: " + exper_info_file)
+    heat_file = args.heat_file
+    print("Heat file: " + heat_file)
+
+    exper_info = ITCExperiment(exper_info_file)
+    actual_q_micro_cal = load_heat_micro_cal(heat_file)
+    actual_q_cal = actual_q_micro_cal * 10**(-6)
+
+    stated_P0 = exper_info.get_cell_concentration_milli_molar()
+    stated_Ls = exper_info.get_syringe_concentration_milli_molar()
+
+    dP0 = args.dP0
+    dLs = args.dLs
+
+    uniform_P0 = args.uniform_P0
+    uniform_Ls = args.uniform_Ls
+    concentration_range_factor = args.concentration_range_factor
+
+    nsamples = args.nsamples
+    nburn = args.nburn
+    nthin = args.nthin
+
+    out = args.out
+
+    all_traces = sample_priors(nsamples, nburn, nthin,
+                               actual_q_cal,
+                               stated_P0, stated_Ls, dP0=dP0, dLs=dLs,
+                               uniform_P0=uniform_P0, uniform_Ls=uniform_Ls,
+                               concentration_range_factor=concentration_range_factor)
+
+    pickle.dump(all_traces, open(out, "w"))
+
+    print("DONE")
