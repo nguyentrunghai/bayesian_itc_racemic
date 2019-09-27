e42b094d24054dd14b1d8e9388c620f8dd849b6e
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-09-27 22:34:05 +0700

    calculate three bayes factors

--- scripts/run_cal_bayes_factor_sample_from_priors.py
+++ scripts/run_cal_bayes_factor_sample_from_priors.py
@@ -13,19 +13,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from _data_io import ITCExperiment, load_heat_micro_cal
-from _bayes_factor import average_likelihood_TwoComponentBindingModel, average_likelihood_RacemicMixtureBindingModel
+from _bayes_factor import average_likelihood_TwoComponentBindingModel
+from _bayes_factor import average_likelihood_RacemicMixtureBindingModel
+from _bayes_factor import average_likelihood_EnantiomerBindingModel
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--racemic_mixture_mcmc_dir", type=str, default="6.racemicmixture_mcmc")
-parser.add_argument("--two_component_mcmc_dir", type=str, default="5.twocomponent_mcmc")
+parser.add_argument("--prior_mcmc_dir", type=str, default="8.prior_mcmc")
+parser.add_argument("--exper_info_dir", type=str, default="6.racemicmixture_mcmc")
 
 parser.add_argument("--heat_dir", type=str, default="4.heat_in_origin_format")
 
 parser.add_argument("--exper_info_file", type=str, default="experimental_information.pickle")
 parser.add_argument("--mcmc_trace_file", type=str, default="traces.pickle")
 
-parser.add_argument("--experiments", type=str,
-                    default="Fokkens_1_c Fokkens_1_d")
+parser.add_argument("--experiments", type=str, default="Fokkens_1_c Fokkens_1_d")
 
 parser.add_argument("--font_scale", type=float, default=0.75)
 parser.add_argument("--xlabel", type=str, default="log[Bayes factor]")
@@ -37,41 +38,50 @@ KB = 0.0019872041      # in kcal/mol/K
 
 experiments = args.experiments.split()
 
-bayes_factors = {}
+bf_rmbm_vs_2cbm = {}
+bf_embm_vs_2cbm = {}
+bf_embm_vs_rmbm = {}
+
 for experiment in experiments:
     print(experiment)
     actual_heat_micro_cal = load_heat_micro_cal(os.path.join(args.heat_dir, experiment + ".DAT"))
 
-    exper_info_rmbm = ITCExperiment(os.path.join(args.racemic_mixture_mcmc_dir,
-                                                 experiment, args.exper_info_file))
-    exper_info_2cbm = ITCExperiment(os.path.join(args.two_component_mcmc_dir,
-                                                 experiment, args.exper_info_file))
+    exper_info = ITCExperiment(os.path.join(args.exper_info_dir, experiment, args.exper_info_file))
 
-    trace_rmbm = pickle.load(open(os.path.join(args.racemic_mixture_mcmc_dir,
-                                               experiment, args.mcmc_trace_file)))
-    trace_2cbm = pickle.load(open(os.path.join(args.two_component_mcmc_dir,
-                                                        experiment, args.mcmc_trace_file)))
-
-    aver_likelihood_rmbm = average_likelihood_RacemicMixtureBindingModel(actual_heat_micro_cal,
-                                                                         V0=exper_info_rmbm.get_cell_volume_liter(),
-                                                                         DeltaVn=exper_info_rmbm.get_injection_volumes_liter(),
-                                                                         beta=1/KB/exper_info_rmbm.get_target_temperature_kelvin(),
-                                                                         n_injections=exper_info_rmbm.get_number_injections(),
-                                                                         mcmc_trace=trace_rmbm)
+    traces = pickle.load(open(os.path.join(args.prior_mcmc_dir, experiment, args.mcmc_trace_file)))
 
     aver_likelihood_2cbm = average_likelihood_TwoComponentBindingModel(actual_heat_micro_cal,
-                                                                       V0=exper_info_2cbm.get_cell_volume_liter(),
-                                                                       DeltaVn=exper_info_2cbm.get_injection_volumes_liter(),
-                                                                       beta=1 / KB / exper_info_2cbm.get_target_temperature_kelvin(),
-                                                                       n_injections=exper_info_2cbm.get_number_injections(),
-                                                                       mcmc_trace=trace_2cbm)
+                                                                       V0=exper_info.get_cell_volume_liter(),
+                                                                       DeltaVn=exper_info.get_injection_volumes_liter(),
+                                                                       beta=1 / KB / exper_info.get_target_temperature_kelvin(),
+                                                                       n_injections=exper_info.get_number_injections(),
+                                                                       mcmc_trace=traces)
 
-    bayes_factor = aver_likelihood_rmbm / aver_likelihood_2cbm
-    bayes_factors[experiment] = bayes_factor
+    aver_likelihood_rmbm = average_likelihood_RacemicMixtureBindingModel(actual_heat_micro_cal,
+                                                                         V0=exper_info.get_cell_volume_liter(),
+                                                                         DeltaVn=exper_info.get_injection_volumes_liter(),
+                                                                         beta=1/KB/exper_info.get_target_temperature_kelvin(),
+                                                                         n_injections=exper_info.get_number_injections(),
+                                                                         mcmc_trace=traces)
 
-    print("aver_likelihood_rmbm: %0.5e" % aver_likelihood_rmbm)
+    aver_likelihood_embm = average_likelihood_EnantiomerBindingModel(actual_heat_micro_cal,
+                                                                     V0=exper_info.get_cell_volume_liter(),
+                                                                     DeltaVn=exper_info.get_injection_volumes_liter(),
+                                                                     beta=1 / KB / exper_info.get_target_temperature_kelvin(),
+                                                                     n_injections=exper_info.get_number_injections(),
+                                                                     mcmc_trace=traces)
+
+    bf_rmbm_vs_2cbm[experiment] = aver_likelihood_rmbm / aver_likelihood_2cbm
+    bf_embm_vs_2cbm[experiment] = aver_likelihood_embm / aver_likelihood_2cbm
+    bf_embm_vs_rmbm[experiment] = aver_likelihood_embm / aver_likelihood_rmbm
+
     print("aver_likelihood_2cbm: %0.5e" % aver_likelihood_2cbm)
-    print("Bayes factor: %0.5e" % bayes_factor)
+    print("aver_likelihood_rmbm: %0.5e" % aver_likelihood_rmbm)
+    print("aver_likelihood_embm: %0.5e" % aver_likelihood_embm)
+    print("Bayes factor rmbm vs 2cbm: %0.5e" % bf_rmbm_vs_2cbm[experiment])
+    print("Bayes factor embm vs 2cbm: %0.5e" % bf_embm_vs_2cbm[experiment])
+    print("Bayes factor embm vs rmbm: %0.5e" % bf_embm_vs_rmbm[experiment])
+    print("")
     print("")
 
 
