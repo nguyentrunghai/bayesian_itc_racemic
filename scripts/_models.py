2cd0b862c785271aaa5cc23fefa2ce8c9e479f4c
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-09-27 10:53:59 +0700

    move average_likelihood_RacemicMixtureBindingModel from _models.py to _bayes_factor.py

--- scripts/_models.py
+++ scripts/_models.py
@@ -174,44 +174,6 @@ def heats_RacemicMixtureBindingModel(V0, DeltaVn, P0, Ls, rho, DeltaH1, DeltaH2,
     return np.array(q_n)
 
 
-
-def average_likelihood_RacemicMixtureBindingModel(q_actual, V0, DeltaVn, beta, n_injections, mcmc_trace):
-    """
-    :param q_actual: observed heats, (micro calorie)
-    :param V0: cell volume (liter)
-    :param DeltaVn: injection volumes (liter)
-    :param beta: inverse temperature * gas constant (mole / kcal)
-    :param n_injections: int
-    :param mcmc_trace: dict, "parameter" --> 1d ndarray
-    :return: aver_likelihood, float
-    """
-    P0_trace = mcmc_trace["P0"]
-    Ls_trace = mcmc_trace["Ls"]
-    rho_trace = mcmc_trace["rho"]
-    DeltaG1_trace = mcmc_trace["DeltaG1"]
-    DeltaDeltaG_trace = mcmc_trace["DeltaDeltaG"]
-    DeltaH1_trace = mcmc_trace["DeltaH1"]
-    DeltaH2_trace = mcmc_trace["DeltaH2"]
-    DeltaH_0_trace = mcmc_trace["DeltaH_0"]
-    log_sigma_trace = mcmc_trace["log_sigma"]
-
-    aver_likelihood = 0.
-    for P0, Ls, rho, DeltaG1, DeltaDeltaG, DeltaH1, DeltaH2, DeltaH_0, log_sigma in zip(P0_trace, Ls_trace, rho_trace,
-                                                                                        DeltaG1_trace, DeltaDeltaG_trace,
-                                                                                        DeltaH1_trace, DeltaH2_trace,
-                                                                                        DeltaH_0_trace, log_sigma_trace):
-        q_model_cal = heats_RacemicMixtureBindingModel(V0, DeltaVn, P0, Ls, rho, DeltaH1, DeltaH2, DeltaH_0,
-                                                       DeltaG1, DeltaDeltaG, beta, n_injections)
-        q_model_micro_cal = q_model_cal * 10. ** 6
-
-        sigma_cal = np.exp(log_sigma)
-        sigma_micro_cal = sigma_cal * 10 ** 6
-
-        aver_likelihood += normal_likelihood(q_actual, q_model_micro_cal, sigma_micro_cal)
-
-    return aver_likelihood / len(P0_trace)
-
-
 def lognormal_pdf(x, stated_center, uncertainty):
     """
     :param x: float
