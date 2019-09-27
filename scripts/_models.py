363e01a9e6af6efa2b9f4fd017f406d96cccbacd
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-09-27 11:02:34 +0700

    move normal_likelihood from _bayes_factor.py to _models.py

--- scripts/_models.py
+++ scripts/_models.py
@@ -174,6 +174,25 @@ def heats_RacemicMixtureBindingModel(V0, DeltaVn, P0, Ls, rho, DeltaH1, DeltaH2,
     return np.array(q_n)
 
 
+def normal_likelihood(q_actual, q_model, sigma):
+    """
+    :param q_actual: 1d ndarray, actual or observed values of heats
+    :param q_model: heat calculated from a model
+    :param sigma: standard deviation
+    :return: likelihood, float
+
+    log_likelihood = -(N/2)\ln(2 \pi \sigma^2) - 1/(2 \sigma^2) \sum_{i=1}^N \epsilon^2
+    """
+    assert len(q_actual) == len(q_model), "q_actual and q_model must have the same len"
+    sum_e_squared = np.sum((q_model - q_actual)**2)
+
+    n_injections = len(q_actual)
+    sigma_2 = sigma**2
+    log_likelihood = - n_injections / 2 * np.log(2 * np.pi * sigma_2) - sum_e_squared / 2 / sigma_2
+
+    return np.exp(log_likelihood)
+
+
 def lognormal_pdf(x, stated_center, uncertainty):
     """
     :param x: float
