7d10aa1e858a1ec17f091faa2fa6b2b73b2ac0d3
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-10-18 21:20:03 +0700

    add mean_square_error()

--- scripts/_optimization.py
+++ scripts/_optimization.py
@@ -23,7 +23,6 @@ def log_likelihood(q_actual, q_model, sigma):
 
     log_likelihood = -(N/2)\ln(2 \pi \sigma^2) - 1/(2 \sigma^2) \sum_{i=1}^N \epsilon^2
     """
-    assert len(q_actual) == len(q_model), "q_actual and q_model must have the same len"
     sum_e_squared = np.sum((q_model - q_actual)**2)
 
     n_injections = len(q_actual)
@@ -31,6 +30,16 @@ def log_likelihood(q_actual, q_model, sigma):
     log_llh = - n_injections / 2. * np.log(2 * np.pi * sigma_2) - sum_e_squared / 2. / sigma_2
 
     return log_llh
+
+
+def mean_square_error(q_actual, q_model):
+    """
+    :param q_actual: 1d ndarray, actual or observed values of heats
+    :param q_model: heat calculated from a model
+    :return mse: float
+    """
+    mse = np.mean((q_model - q_actual)**2)
+    return mse
 
 
 def log_lognormal(x, stated_center, uncertainty):
