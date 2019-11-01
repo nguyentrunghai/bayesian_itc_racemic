bea97482bd4e31894c58793dc7759b222edf4396
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-11-01 16:15:14 +0700

    fix bug

--- scripts/_pymc3_models.py
+++ scripts/_pymc3_models.py
@@ -20,8 +20,7 @@ def lognormal_prior(name, stated_value, uncertainty):
     v = uncertainty ** 2
     return pymc3.Lognormal(name,
                            mu=np.log(m / np.sqrt(1 + (v / (m ** 2)))),
-                           tau=1.0 / np.log(1 + (v / (m ** 2))),
-                           value=m)
+                           tau=1.0 / np.log(1 + (v / (m ** 2))))
 
 
 def uniform_prior(name, lower, upper):
