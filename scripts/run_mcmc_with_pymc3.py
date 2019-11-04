fa905887157a0777f31dca2504824c950b2cefdf
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-11-04 14:49:15 +0700

    plot trace

--- scripts/run_mcmc_with_pymc3.py
+++ scripts/run_mcmc_with_pymc3.py
@@ -3,6 +3,9 @@ Run sampling of the posterior
 """
 
 import pymc3
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 from _data_io import ITCExperiment, load_heat_micro_cal
 from _pymc3_models import make_TwoComponentBindingModel
@@ -18,3 +21,12 @@ q_actual_cal = q_actual_micro_cal * 10.**(-6)
 model = make_TwoComponentBindingModel(q_actual_cal, exper_info,
                                       dcell=0.1, dsyringe=0.1,
                                       uniform_P0=False, uniform_Ls=False, concentration_range_factor=10)
+
+with model:
+    trace = pymc3.sample()
+
+
+plt.figure()
+pymc3.traceplot(trace)
+plt.tight_layout()
+plt.savefig("test.pdf")
