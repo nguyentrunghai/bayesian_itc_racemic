1901be1205bf2c968060d3ed250a10f5de4e6371
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-11-19 08:56:38 +0700

    add new file

---
+++ scripts/run_plot_bayes_factor.py
@@ -0,0 +1,13 @@
+"""
+To collect and plot Bayes factors from sequential MC results
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--two_component_mcmc_dir", type=str, default="twocomponent_mcmc")
+parser.add_argument("--racemic_mixture_mcmc_dir", type=str, default="racemicmixture_mcmc")
+parser.add_argument("--enantiomer_mcmc_dir", type=str, default="enantiomer_mcmc")
+
+args = parser.parse_args()
