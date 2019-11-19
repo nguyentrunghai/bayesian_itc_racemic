607108a36ce79e5467c610a9a2ff218ea81030f3
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-11-19 10:34:58 +0700

    calculate bayes factors

--- scripts/run_plot_bayes_factor.py
+++ scripts/run_plot_bayes_factor.py
@@ -1,6 +1,7 @@
 """
 To collect and plot Bayes factors from sequential MC results
 """
+from __future__ import print_function
 
 import argparse
 import glob
@@ -32,19 +33,37 @@ print("racemic_mixture_dir:", racemic_mixture_dirs)
 enantiomer_dirs = glob.glob(os.path.join(args.enantiomer_mcmc_dir, args.repeat_prefix + "*"))
 print("enantiomer_dir:", enantiomer_dirs)
 
-two_component_bf = defaultdict(list)
-racemic_mixture_bf = defaultdict(list)
-enantiomer_bf = defaultdict(list)
+two_component_ml = defaultdict(list)
+racemic_mixture_ml = defaultdict(list)
+enantiomer_ml = defaultdict(list)
 
 for experiment in experiments:
     for repeat_dir in two_component_dirs:
         bf_file = os.path.join(repeat_dir, experiment, args.bayes_factor_file)
-        two_component_bf[experiment].append(np.loadtxt(bf_file))
+        two_component_ml[experiment].append(np.loadtxt(bf_file))
 
     for repeat_dir in racemic_mixture_dirs:
         bf_file = os.path.join(repeat_dir, experiment, args.bayes_factor_file)
-        racemic_mixture_bf[experiment].append(np.loadtxt(bf_file))
+        racemic_mixture_ml[experiment].append(np.loadtxt(bf_file))
 
     for repeat_dir in enantiomer_dirs:
         bf_file = os.path.join(repeat_dir, experiment, args.bayes_factor_file)
-        enantiomer_bf[experiment].append(np.loadtxt(bf_file))
+        enantiomer_ml[experiment].append(np.loadtxt(bf_file))
+
+
+bf_rmbm_vs_2cbm = defaultdict(list)
+bf_embm_vs_2cbm = defaultdict(list)
+bf_embm_vs_rmbm = defaultdict(list)
+
+for experiment in experiments:
+    for bf_rmbm in racemic_mixture_ml[experiment]:
+        for bf_2cbm in two_component_ml[experiment]:
+            bf_rmbm_vs_2cbm[experiment].append(bf_rmbm / bf_2cbm)
+
+    for bf_embm in enantiomer_ml[experiment]:
+        for bf_2cbm in two_component_ml[experiment]:
+            bf_embm_vs_2cbm[experiment].append(bf_embm / bf_2cbm)
+
+    for bf_embm in enantiomer_ml[experiment]:
+        for bf_rmbm in racemic_mixture_ml[experiment]:
+            bf_embm_vs_rmbm[experiment].append(bf_embm / bf_rmbm)
