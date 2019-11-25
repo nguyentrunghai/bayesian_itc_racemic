f45aaa58d8b1f1ef932d844c2e8b908ca71be0b7
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-11-25 09:16:33 +0700

    add function marginal_lhs_bootstrap

--- scripts/_bayes_factor.py
+++ scripts/_bayes_factor.py
@@ -4,3 +4,24 @@ define function to calculate bayes factor
 
 from __future__ import print_function
 
+import numpy as np
+
+from _models import marginal_likelihood
+
+
+def marginal_lhs_bootstrap(extracted_loglhs, sample_size=None, bootstrap_repeats=1):
+    """
+    :param extracted_loglhs: 1d array
+    :param sample_size: int
+    :param bootstrap_repeats: int
+    :return: all_sample_estimate, bootstrap_mean, bootstrap_std
+    """
+    all_sample_estimate = marginal_likelihood(extracted_loglhs)
+
+    bootstrap_samples = []
+    for _ in range(bootstrap_repeats):
+        drawn_loglhs = np.random.choice(extracted_loglhs, size=sample_size, replace=True)
+        bootstrap_samples.append(drawn_loglhs)
+
+    return all_sample_estimate, bootstrap_samples.mean(), bootstrap_samples.std()
+
