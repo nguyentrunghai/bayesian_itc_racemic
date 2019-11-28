19aa176e2ced51d0c6fde0becbe5bd22e6e74972
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-11-28 21:29:35 +0700

    change from estimating marginal likelihood to log marginal likelihood

--- scripts/_bayes_factor.py
+++ scripts/_bayes_factor.py
@@ -6,10 +6,10 @@ from __future__ import print_function
 
 import numpy as np
 
-from _models import marginal_likelihood
+from _models import log_marginal_likelihood
 
 
-def marginal_lhs_bootstrap(extracted_loglhs, sample_size=None, bootstrap_repeats=1):
+def log_marginal_lhs_bootstrap(extracted_loglhs, sample_size=None, bootstrap_repeats=1):
     """
     :param extracted_loglhs: 1d array
     :param sample_size: int
@@ -18,13 +18,13 @@ def marginal_lhs_bootstrap(extracted_loglhs, sample_size=None, bootstrap_repeats
     """
     if sample_size is None:
         sample_size = len(extracted_loglhs)
-    all_sample_estimate = marginal_likelihood(extracted_loglhs)
+    all_sample_estimate = log_marginal_likelihood(extracted_loglhs)
 
     bootstrap_samples = []
     for _ in range(bootstrap_repeats):
         drawn_loglhs = np.random.choice(extracted_loglhs, size=sample_size, replace=True)
 
-        bootstrap_samples.append(marginal_likelihood(drawn_loglhs))
+        bootstrap_samples.append(log_marginal_likelihood(drawn_loglhs))
 
     bootstrap_samples = np.array(bootstrap_samples)
 
