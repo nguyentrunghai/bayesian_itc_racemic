717d66d72d9b5677c74334832c3099874f9d27bb
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-02-19 21:21:10 +0700

    add func dic_2

--- scripts/run_plot_information_criterion.py
+++ scripts/run_plot_information_criterion.py
@@ -55,10 +55,43 @@ def dic_1(traces, log_llhs,
     :return: llhs, 1d array
     """
     posterior_mean = {var_name: [np.mean(var_trace)] for var_name, var_trace in traces.items()}
-    _, log_llh = extract_loglhs_from_traces_pymc3(posterior_mean, model_name, exper_info_file, heat_file,
-                                     dcell=dcell, dsyringe=dsyringe,
-                                     uniform_P0=uniform_P0, uniform_Ls=uniform_Ls,
-                                     concentration_range_factor=concentration_range_factor,
-                                     auto_transform=auto_transform)
-    log_llh = log_llh[0]
-    return 2 * log_llh - 2 * np.mean(log_llhs)
+    _, log_llh_bayes = extract_loglhs_from_traces_pymc3(posterior_mean, model_name, exper_info_file, heat_file,
+                                                        dcell=dcell, dsyringe=dsyringe,
+                                                        uniform_P0=uniform_P0, uniform_Ls=uniform_Ls,
+                                                        concentration_range_factor=concentration_range_factor,
+                                                        auto_transform=auto_transform)
+    log_llh_bayes = log_llh_bayes[0]
+    return 2 * log_llh_bayes - 2 * np.mean(log_llhs)
+
+
+def dic_2(traces, log_llhs,
+          model_name, exper_info_file, heat_file,
+          dcell=0.1, dsyringe=0.1,
+          uniform_P0=False, uniform_Ls=False,
+          concentration_range_factor=10.,
+          auto_transform=False):
+    """
+    dic_1 = 2 \ln p(y|\hat{\theta}_{Bayes}) - 2 \frac{1}{S} \sum_{s=1}^S \ln p(y|\theta^s)
+    where \hat{\theta}_{Bayes} is mean of the posterior
+
+    :param traces: dict, variable_name -> 1d array
+    :param log_llhs: array-like of float, log likelihood values
+    :param model_name: str, in ["2cbm", "rmbm", "embm"]
+    :param exper_info_file: str
+    :param heat_file: str
+    :param dcell: float in (0, 1)
+    :param dsyringe: float in (0, 1)
+    :param uniform_P0: bool
+    :param uniform_Ls: bool
+    :param concentration_range_factor: float
+    :param auto_transform: bool
+    :return: llhs, 1d array
+    """
+    posterior_mean = {var_name: [np.mean(var_trace)] for var_name, var_trace in traces.items()}
+    _, log_llh_bayes = extract_loglhs_from_traces_pymc3(posterior_mean, model_name, exper_info_file, heat_file,
+                                                        dcell=dcell, dsyringe=dsyringe,
+                                                        uniform_P0=uniform_P0, uniform_Ls=uniform_Ls,
+                                                        concentration_range_factor=concentration_range_factor,
+                                                        auto_transform=auto_transform)
+    log_llh_bayes = log_llh_bayes[0]
+    return -2 * log_llh_bayes + 4 * np.var(log_llhs)
