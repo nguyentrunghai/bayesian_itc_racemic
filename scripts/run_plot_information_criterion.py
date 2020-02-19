65d2b350077adef125300fd5b8e92067b4d833bf
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-02-19 19:51:30 +0700

    add func bic

--- scripts/run_plot_information_criterion.py
+++ scripts/run_plot_information_criterion.py
@@ -5,13 +5,26 @@ DIC (deviance information criterion)
 
 import numpy as np
 
+
 def aic(log_llhs, k):
     """
-    aci = -2 log p(y | \theta_{MLE}) + 2k
+    aic = -2 log p(y | \theta_{MLE}) + 2k
     Ref: Andrew Gelman et al., "Bayesian Data Analysis", 3rd Ed., CRC Press, page 169
     :param log_llhs: array-like of float, log likelihood values
     :param k: number of parameters
     :return: float, Akaike information criterion
     """
     return -2 * np.max(log_llhs) + 2 * k
+
+
+def bic(log_llhs, n, k):
+    """
+    bic = -2 log p(y | \theta_{MLE}) + ln(n)k
+    Ref: Andrew Gelman et al., "Bayesian Data Analysis", 3rd Ed., CRC Press, page 169
+    :param log_llhs: array-like of float, log likelihood values
+    :param n: int, number of paramters
+    :param k: number of parameters
+    :return: float, Bayesian information criterion
+    """
+    return -2 * np.max(log_llhs) + np.log(n) * k
 
