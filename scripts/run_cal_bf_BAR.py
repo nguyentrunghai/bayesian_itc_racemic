feb2c90b0c48d0293f527777e86d74b807b73f72
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-03-21 21:50:04 +0700

    add new file

---
+++ scripts/run_cal_bf_BAR.py
@@ -0,0 +1,28 @@
+"""
+calculate Bayes factors using BAR estimator
+"""
+
+from __future__ import print_function
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--two_component_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/07.twocomponent_mcmc/pymc3_met_2/repeat_0")
+parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/08.racemicmixture_mcmc/pymc3_met_2/repeat_0")
+parser.add_argument("--enantiomer_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/09.enantiomer_mcmc/pymc3_met_2/repeat_0")
+
+parser.add_argument("--model_pickle", type=str, default="pm_model.pickle")
+parser.add_argument("--trace_pickle", type=str, default="trace_obj.pickle")
+
+parser.add_argument("--experiments", type=str,
+default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
+
+args = parser.parse_args()
+
+experiments = args.experiments.split()
+print("experiments:", experiments)
