bf13a203c8133009ede332226f492e1f720ad297
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-03-23 08:03:47 +0700

    fix bug

--- scripts/run_test_BAR_for_bayes_factors.py
+++ scripts/run_test_BAR_for_bayes_factors.py
@@ -8,7 +8,7 @@ import numpy as np
 def generate_X1(size=100, random_state=123):
     rand = np.random.RandomState(random_state)
     mu = rand.normal(loc=10, scale=3, size=size)
-    x1 = [rand.normal(loc=mu[i], scale=4) for i in mu]
+    x1 = [rand.normal(loc=m, scale=4) for m in mu]
     return np.array(x1)
 
 
