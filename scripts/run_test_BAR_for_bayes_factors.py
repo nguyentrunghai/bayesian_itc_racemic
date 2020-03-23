9cd4033359f99597e06fb4742f7bfc735b931222
Trung Haii Nguyen <nguyentrungh@gmail.com>
2020-03-23 07:55:28 +0700

    add new file

---
+++ scripts/run_test_BAR_for_bayes_factors.py
@@ -0,0 +1,14 @@
+"""
+Test BAR estimator for calculating Bayes factors for simple model
+"""
+
+import numpy as np
+
+
+def generate_X1(size=100, random_state=123):
+    rand = np.random.RandomState(random_state)
+    mu = rand.normal(loc=10, scale=3, size=size)
+    x1 = [rand.normal(loc=mu[i], scale=4) for i in mu]
+    return np.array(x1)
+
+
