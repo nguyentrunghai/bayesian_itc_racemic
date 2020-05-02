2558d63067d4f21d3c47f518699fd79fec80daf7
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-05-02 09:56:24 +0700

    add new file

---
+++ scripts/run_plot_heats_MAP_CI.py
@@ -0,0 +1,14 @@
+"""
+plot heat curves with confidence interval bands
+"""
+
+import argparse
+import os
+import pickle
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
