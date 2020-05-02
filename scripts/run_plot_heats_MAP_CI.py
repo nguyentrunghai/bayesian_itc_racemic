3ba29a39357daa2e5bc6c7971c040871efc2b06a
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-05-02 12:48:13 +0700

    plot heats

--- scripts/run_plot_heats_MAP_CI.py
+++ scripts/run_plot_heats_MAP_CI.py
@@ -18,6 +18,8 @@ parser.add_argument("--data_dir", type=str, default="./")
 parser.add_argument("--experiments", type=str,
 default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
 
+parser.add_argument("--ci_level", type=float, default=95)
+
 parser.add_argument("--font_scale", type=float, default=0.75)
 parser.add_argument("--xlabel", type=str, default="# injections")
 parser.add_argument("--ylabel", type=str, default="heat ($\mu$cal)")
@@ -73,3 +75,37 @@ def plot_heats(q_actual,
     fig.savefig(out, dpi=300)
 
     return None
+
+
+experiments = args.experiments.split()
+print("experiments:", experiments)
+ci_level = args.ci_level
+ci_label = "%d%% CI" % (int(ci_level))
+
+for exper in experiments:
+    infile = os.path.join(args.data_dir, exper + ".pickle")
+    print("Loading: " + infile)
+    with open(infile, "rb") as handle:
+        data = pickle.load(handle)
+
+    q_actual = data["q_actual"]
+    q_map_2c = data["q_map_2c"]
+    q_map_rm = data["q_map_rm"]
+    q_map_em = data["q_map_em"]
+
+    lowers_2c, uppers_2c = conf_interv(data["qs_2c"], conf_level=ci_level)
+    lowers_rm, uppers_rm = conf_interv(data["qs_rm"], conf_level=ci_level)
+    lowers_em, uppers_em = conf_interv(data["qs_em"], conf_level=ci_level)
+
+    out = exper + ".pdf"
+
+    plot_heats(q_actual,
+               q_map_2c, lowers_2c, uppers_2c,
+               q_map_rm, lowers_rm, uppers_rm,
+               q_map_em, lowers_em, uppers_em,
+               font_scale=args.font_scale,
+               xlabel=args.xlabel, ylabel=args.ylabel,
+               ci_label=ci_label,
+               out=out)
+
+print("DONE")
