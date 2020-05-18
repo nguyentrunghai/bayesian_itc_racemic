44ea5d65bc278351708e33df22505a30987f4ce9
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-05-18 15:54:39 +0700

    plot DG for 2C

--- scripts/run_plot_histograms_1d.py
+++ scripts/run_plot_histograms_1d.py
@@ -32,6 +32,8 @@ parser.add_argument("--model_pickle", type=str, default="pm_model.pickle")
 parser.add_argument("--experiments", type=str,
 default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
 
+parser.add_argument("--font_scale", type=float, default=0.75)
+
 args = parser.parse_args()
 
 
@@ -123,6 +125,8 @@ print("experiments", experiments)
 colors = ("r", "b", "g")
 ylabel = "Probability density"
 
+font_scale = args.font_scale
+
 for exper in experiments:
     print("\n\n", exper)
 
@@ -149,6 +153,10 @@ for exper in experiments:
     traces_rm = [pickle.load(open(os.path.join(d, args.trace_pickle))) for d in dirs_rm]
     traces_em = [pickle.load(open(os.path.join(d, args.trace_pickle))) for d in dirs_em]
 
+    tr_val_2c = value_from_traces(traces_2c)
+    tr_val_rm = value_from_traces(traces_rm)
+    tr_val_em = value_from_traces(traces_em)
+
     map_2c = find_MAP_traces(model_2c, traces_2c)
     print("map_2c", map_2c)
 
@@ -163,61 +171,14 @@ for exper in experiments:
     plt.subplots_adjust(wspace=0.02)
     sns.set(font_scale=font_scale)
 
+    # 2c
+    ax = axes[0]
+    xs = [tr_val_2c["DeltaG"]]
+    maps = [map_2c["DeltaG"]]
+    cis = [conf_interv(x) for x in xs]
+    labels = ["$\Delta G$"]
+    plot_kde_hist(xs, labels, colors, ax)
+    plot_conf_intervs(cis, colors, ax)
+    plot_maps(maps, colors, ax)
 
-for experiment in experiments:
-    print("Processing " + experiment)
-    twocomponent_traces_file = os.path.join(args.twocomponent_mcmc_dir, experiment, args.mcmc_trace_file)
-    print("Loading " + twocomponent_traces_file)
-    twocomponent_traces = pickle.load(open(twocomponent_traces_file))
-
-    alter_model_traces_file = os.path.join(args.alternative_model_mcmc_dir, experiment, args.mcmc_trace_file)
-    print("Loading " + alter_model_traces_file)
-    alter_model_trace = pickle.load(open(alter_model_traces_file))
-
-    # DeltaG
-    print("Ploting DeltaG")
-    DeltaG1 = alter_model_trace["DeltaG1"]
-    DeltaG2 = alter_model_trace["DeltaG1"] + alter_model_trace["DeltaDeltaG"]
-    data_list = (twocomponent_traces["DeltaG"], DeltaG1, DeltaG2)
-    labels = ("$\Delta G$ (TwoComponent)", "$\Delta G_1$", "$\Delta G_2$")
-    xlabel = "$\Delta G$ (kcal/mol)"
-    out = experiment + "_" + args.alternative_model + "_deltaG" + ".pdf"
-    _plot_kde_hist(data_list, labels, colors, xlabel, ylabel, out)
-
-    # DeltaH
-    print("Ploting DeltaH")
-    DeltaH1 = alter_model_trace["DeltaH1"]
-    DeltaH2 = alter_model_trace["DeltaH2"]
-    data_list = (twocomponent_traces["DeltaH"], DeltaH1, DeltaH2)
-    labels = ("$\Delta H$ (TwoComponent)", "$\Delta H_1$", "$\Delta H_2$")
-    xlabel = "$\Delta H$ (kcal/mol)"
-    out = experiment + "_" + args.alternative_model + "_deltaH" + ".pdf"
-    _plot_kde_hist(data_list, labels, colors, xlabel, ylabel, out)
-
-    # Ls
-    print("Ploting Ls")
-    if args.alternative_model == "racemicmixture":
-        rho = 0.5
-    elif args.alternative_model == "enantiomer":
-        rho = alter_model_trace["rho"]
-
-    Ls1 = alter_model_trace["Ls"] * rho
-    Ls2 = alter_model_trace["Ls"] * (1 - rho)
-    data_list = (twocomponent_traces["Ls"], Ls1, Ls2)
-    labels = ("$[L]_s$ (TwoComponent)", "$[L_1]_s$", "$[L_2]_s$")
-    xlabel = "$[L]_s$ (mM)"
-    out = experiment + "_" + args.alternative_model + "_Ls" + ".pdf"
-    _plot_kde_hist(data_list, labels, colors, xlabel, ylabel, out)
-
-    # P0
-    print("Ploting P0")
-    data_list = (twocomponent_traces["P0"], alter_model_trace["P0"])
-
-    if args.alternative_model == "racemicmixture":
-        labels = ("$[R]_0$ (TwoComponent)", "$[R]_0$ (RacemicMixture)")
-    elif args.alternative_model == "enantiomer":
-        labels = ("$[R]_0$ (TwoComponent)", "$[R]_0$ (Enantiomer)")
-
-    xlabel = "$[R]_0$ (mM)"
-    out = experiment + "_" + args.alternative_model + "_P0" + ".pdf"
-    _plot_kde_hist(data_list, labels, colors[:-1], xlabel, ylabel, out)
+    
