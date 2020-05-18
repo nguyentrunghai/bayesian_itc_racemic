d3ed8b88f0efe0024db45bb80210747fd78e4732
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-05-18 13:57:30 +0700

    add arguments

--- scripts/run_plot_histograms_1d.py
+++ scripts/run_plot_histograms_1d.py
@@ -11,15 +11,21 @@ import seaborn as sns
 sns.set()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--twocomponent_mcmc_dir", type=str, default="5.twocomponent_mcmc")
+parser.add_argument("--two_component_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/07.twocomponent_mcmc/pymc3_met_2")
+parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/08.racemicmixture_mcmc/pymc3_met_2")
+parser.add_argument("--enantiomer_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/09.enantiomer_mcmc/pymc3_met_2")
 
-# racemicmixture or enantiomer
-parser.add_argument("--alternative_model", type=str, default="racemicmixture")
-parser.add_argument("--alternative_model_mcmc_dir", type=str, default="6.racemicmixture_mcmc")
+parser.add_argument("--repeat_prefix", type=str, default="repeat_")
+parser.add_argument("--exclude_repeats", type=str, default="")
 
+parser.add_argument("--trace_pickle", type=str, default="trace_obj.pickle")
 
-parser.add_argument("--mcmc_trace_file", type=str, default="traces.pickle")
-parser.add_argument("--experiments", type=str, default="Fokkens_1_c Fokkens_1_d Fokkens_1_e")
+parser.add_argument("--experiments", type=str,
+default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
+
 args = parser.parse_args()
 
 
