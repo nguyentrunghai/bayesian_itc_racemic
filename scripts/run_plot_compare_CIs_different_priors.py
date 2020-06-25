292c86f7859d632841ffe6f5a376646e13ac5ee3
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-06-25 15:21:51 +0700

    load data for em

--- scripts/run_plot_compare_CIs_different_priors.py
+++ scripts/run_plot_compare_CIs_different_priors.py
@@ -116,3 +116,15 @@ for exper in experiments:
 
         traces_rm.append(trace)
 
+    # load em
+    traces_em = []
+    for prior_dir in prior_dirs:
+
+        dirs = glob.glob(os.path.join(
+            args.enantiomer_dir, prior_dir, args.repeat_prefix + "*", exper, args.trace_pickle))
+        dirs = [os.path.dirname(p) for p in dirs]
+        print("Loading traces from", dirs)
+        trace = [pickle.load(open(os.path.join(d, args.trace_pickle))) for d in dirs]
+
+        traces_em.append(trace)
+
