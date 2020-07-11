f75cc0be38335b684107ec2c172a4e6a24530e3e
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-07-11 21:34:26 +0700

    plot convergence

--- scripts/run_plot_convergence_posterior.py
+++ scripts/run_plot_convergence_posterior.py
@@ -6,8 +6,7 @@ from __future__ import print_function
 
 import argparse
 import os
-
-import numpy as np
+import pandas as pd
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -24,6 +23,8 @@ default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Bau
 parser.add_argument("--percentiles", type=str, default="5 25 50 75 95")
 
 parser.add_argument("--vars", type=str, default="DeltaH DeltaG P0 Ls")
+parser.add_argument("--ylabels", type=str, default="$\Delta H$ $\Delta G$ $[R]_0$ $[L]_s$")
+parser.add_argument("--xlabel", type=str, default="Sample proportion")
 
 parser.add_argument("--font_scale", type=float, default=0.75)
 
@@ -38,8 +39,56 @@ vars = args.vars.split()
 assert len(vars) in [4, 6], "len of vars must be 4 or 6"
 print("vars:", vars)
 
+ylabels = args.ylabels.split()
+assert len(ylabels) == len(vars), "ylables and vars must have the same len"
+
+xlabel = args.xlabel
+
 qs = [float(s) for s in args.percentiles.split()]
-data_cols = ["%0.1-th" %q for q in qs]
+data_cols = ["%0.1f-th" % q for q in qs]
 print("data_cols:", data_cols)
-err_cols = ["%0.1--error" %q for q in qs]
+err_cols = ["%0.1f-error" % q for q in qs]
 print("err_cols:", err_cols)
+legends = ["%d-th" % q for q in qs]
+
+colors = ["b", "g", "r", "c", "m"]
+line_styles = ["solid", "dotted", "dashed", "dashdot", "loosely dashed"]
+
+for exper in experiments:
+    print("\n\nPloting " + exper)
+
+    if len(vars) == 4:
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(6.4, 4.8))
+    elif len(vars) == 6:
+        fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(9.6, 4.8))
+
+    axes = axes.flatten()
+
+    for var, ylabel, ax in zip(vars, ylabels, axes):
+        print(var)
+        print(ylabel)
+        inp_file = os.path.join(args.data_dir, exper + "_" + var + ".dat")
+        print(inp_file)
+        data = pd.read_csv(inp_file, sep="\s+")
+        x = data["proportion"]
+
+        for i, data_col in enumerate(data_cols):
+            err_col = err_cols[i]
+            color = colors[i]
+            line_style = line_styles[i]
+            legend = legends[i]
+
+            y = data[data_col]
+            yerr = data[err_col]
+
+            ax.errorbar(x, y, yerrs=yerr, linestyle=line_style, c=color, label=legend)
+
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.legend(loc="best")
+
+    out = exper + ".pdf"
+    fig.tight_layout()
+    fig.savefig(out, dpi=300)
+
+print("DONE")
