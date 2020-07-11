f6e3059ad339b7a91cc8741574472ebfeeaa631b
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-07-11 14:27:52 +0700

    add random_state arg

--- scripts/run_convergence_posterior_percentiles.py
+++ scripts/run_convergence_posterior_percentiles.py
@@ -25,6 +25,8 @@ parser.add_argument("--vars", type=str, default="DeltaH DeltaG P0 Ls")
 parser.add_argument("--sample_proportions", type=str, default="0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0")
 parser.add_argument("--repeats", type=int, default=10)
 
+parser.add_argument("--random_state", type=int, default=4273)
+
 args = parser.parse_args()
 
 
