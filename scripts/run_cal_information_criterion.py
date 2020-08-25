5f0627957f562ba535b2839a3c780c1905bbfe68
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-08-25 14:43:58 +0700

    parse data dirs

--- scripts/run_cal_information_criterion.py
+++ scripts/run_cal_information_criterion.py
@@ -5,6 +5,7 @@ calculate BIC and WBIC
 from __future__ import print_function
 
 import argparse
+import glob
 import os
 import pickle
 
@@ -26,7 +27,45 @@ parser.add_argument("--exclude_repeats", type=str, default="")
 parser.add_argument("--model_pickle", type=str, default="pm_model.pickle")
 parser.add_argument("--trace_pickle", type=str, default="trace_obj.pickle")
 
+parser.add_argument("--heat_data_dir", type=str, default="/home/tnguye46/bayesian_itc_racemic/04.heat_in_origin_format")
+
 parser.add_argument("--experiments", type=str,
 default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
 
 args = parser.parse_args()
+
+
+def is_path_excluded(path, exclude_kws):
+    for kw in exclude_kws:
+        if kw in path:
+            return True
+    return False
+
+
+exclude_repeats = args.exclude_repeats.split()
+exclude_repeats = [args.repeat_prefix + r for r in exclude_repeats]
+print("exclude_repeats:", exclude_repeats)
+
+experiments = args.experiments.split()
+print("experiments", experiments)
+
+
+for exper in experiments:
+    print("\n\n", exper)
+
+    dirs_2c = glob.glob(os.path.join(args.two_component_mcmc_dir, args.repeat_prefix + "*", exper, args.trace_pickle))
+    dirs_2c = [os.path.dirname(p) for p in dirs_2c]
+    dirs_2c = [p for p in dirs_2c if not is_path_excluded(p, exclude_repeats)]
+    print("dirs_2c:", dirs_2c)
+
+    dirs_rm = glob.glob(os.path.join(args.racemic_mixture_mcmc_dir, args.repeat_prefix + "*", exper, args.trace_pickle))
+    dirs_rm = [os.path.dirname(p) for p in dirs_rm]
+    dirs_rm = [p for p in dirs_rm if not is_path_excluded(p, exclude_repeats)]
+    print("dirs_rm:", dirs_rm)
+
+    dirs_em = glob.glob(os.path.join(args.enantiomer_mcmc_dir, args.repeat_prefix + "*", exper, args.trace_pickle))
+    dirs_em = [os.path.dirname(p) for p in dirs_em]
+    dirs_em = [p for p in dirs_em if not is_path_excluded(p, exclude_repeats)]
+    print("dirs_em:", dirs_em)
+
+    
