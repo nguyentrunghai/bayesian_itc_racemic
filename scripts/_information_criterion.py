cef4374859b6c5ba5b99245fc5466017645f0822
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-08-25 11:27:28 +0700

    add func log_likelihood_trace()

--- scripts/_information_criterion.py
+++ scripts/_information_criterion.py
@@ -7,7 +7,7 @@ https://www.jmlr.org/papers/volume14/watanabe13a/watanabe13a.pdf
 from __future__ import print_function
 
 import pickle
-
+import copy
 import numpy as np
 
 from _bayes_factor import get_values_from_traces
@@ -22,3 +22,21 @@ def get_values_from_trace_files(model, trace_pkl_files, thin=1, burn=0):
 
 def load_model(model_pkl):
     return pickle.load(open(model_pkl))
+
+
+def log_likelihood_trace(model, trace_values):
+
+    model_vars = [var.name for var in model.vars]
+    trace_vars = trace_values.keys()
+
+    trace_v = {}
+    for var in model_vars:
+        if var not in trace_vars:
+            raise KeyError(var + " is not in trace")
+        trace_v[var] = copy.deepcopy(trace_values[var])
+
+    trace_v = dict_to_list(trace_v)
+    obs_q = model.observed_RVs[0]
+    get_logp = np.vectorize(obs_q.logp)
+    logp = get_logp(trace_v)
+    return logp
