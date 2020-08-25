9aadf74e1dc0617e15ceca505c862665e088ca49
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-08-25 11:19:13 +0700

    add func load_model

--- scripts/_information_criterion.py
+++ scripts/_information_criterion.py
@@ -11,10 +11,14 @@ import pickle
 import numpy as np
 
 from _bayes_factor import get_values_from_traces
-from _bayes_factor impprt dict_to_list
+from _bayes_factor import dict_to_list
 
 
 def get_values_from_trace_files(model, trace_pkl_files, thin=1, burn=0):
     traces = [pickle.load(open(trace_file)) for trace_file in trace_pkl_files]
     trace_values = get_values_from_traces(model, traces, thin=thin, burn=burn)
     return trace_values
+
+
+def load_model(model_pkl):
+    return pickle.load(open(model_pkl))
