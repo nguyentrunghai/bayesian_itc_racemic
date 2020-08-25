7bfc0ca6cf814873a8e559962670ceee10e0f8fe
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-08-25 14:54:28 +0700

    load models and traces

--- scripts/run_cal_information_criterion.py
+++ scripts/run_cal_information_criterion.py
@@ -12,6 +12,9 @@ import pickle
 import numpy as np
 import pandas as pd
 
+from _information_criterion import get_n_injections, get_n_params
+from _information_criterion import load_model, get_values_from_trace_files
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--two_component_mcmc_dir", type=str,
@@ -68,4 +71,19 @@ for exper in experiments:
     dirs_em = [p for p in dirs_em if not is_path_excluded(p, exclude_repeats)]
     print("dirs_em:", dirs_em)
 
+    heat_file = os.path.join(args.heat_data_dir, exper + ".DAT")
+    n_injections = get_n_injections(heat_file)
+    print("n_injections:", n_injections)
+
+    model_2c = load_model(os.path.join(dirs_2c[0], args.model_pickle))
+    model_rm = load_model(os.path.join(dirs_rm[0], args.model_pickle))
+    model_em = load_model(os.path.join(dirs_em[0], args.model_pickle))
+
+    trace_files_2c = [os.path.join(d, args.trace_pickle) for d in dirs_2c]
+    trace_files_rm = [os.path.join(d, args.trace_pickle) for d in dirs_rm]
+    trace_files_em = [os.path.join(d, args.trace_pickle) for d in dirs_em]
+
+    traces_2c = get_values_from_trace_files(model_2c, trace_files_2c)
+    traces_rm = get_values_from_trace_files(model_rm, trace_files_rm)
+    traces_em = get_values_from_trace_files(model_rm, trace_files_em)
     
