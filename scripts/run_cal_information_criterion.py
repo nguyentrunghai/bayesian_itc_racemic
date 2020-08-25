41c3b494d73b0f02e01e1758f48d9092839b910c
Trung Hai Nguyen <nguyentrungh@gmail.com>
2020-08-25 14:33:33 +0700

    add new file

---
+++ scripts/run_cal_information_criterion.py
@@ -0,0 +1,32 @@
+"""
+calculate BIC and WBIC
+"""
+
+from __future__ import print_function
+
+import argparse
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--two_component_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/07.twocomponent_mcmc/pymc3_nuts_2")
+parser.add_argument("--racemic_mixture_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/08.racemicmixture_mcmc/pymc3_nuts_2")
+parser.add_argument("--enantiomer_mcmc_dir", type=str,
+                    default="/home/tnguye46/bayesian_itc_racemic/09.enantiomer_mcmc/pymc3_nuts_2")
+
+parser.add_argument("--repeat_prefix", type=str, default="repeat_")
+parser.add_argument("--exclude_repeats", type=str, default="")
+
+parser.add_argument("--model_pickle", type=str, default="pm_model.pickle")
+parser.add_argument("--trace_pickle", type=str, default="trace_obj.pickle")
+
+parser.add_argument("--experiments", type=str,
+default="Fokkens_1_a Fokkens_1_b Fokkens_1_c Fokkens_1_d Fokkens_1_e Baum_57 Baum_59 Baum_60_1 Baum_60_2 Baum_60_3 Baum_60_4")
+
+args = parser.parse_args()
